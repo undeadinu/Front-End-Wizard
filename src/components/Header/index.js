c30dac5347f22a97ed83eb36b5e21303e1525cd2
Kieran Venison <Kieran.Venison@mypd.local>
2018-10-23 09:13:21 +0100

    added moment and new indicator and added some links"

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ const Header = () => (
       <img className="header__img mb-3" src={logo} alt="Front end wizard logo" />
         <h1 className="text-center display-4">Front End Wizard</h1>
         <p className="text-center">
-          <small className="badge badge-primary">v2.0.1</small>
+          <small className="badge badge-primary">v2.0.2</small>
         </p>
     </div>
   </header>
--- src/components/TopicGroup/TopicLink/index.js
+++ src/components/TopicGroup/TopicLink/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import moment from 'moment';
 import {
   faBook,
   faGlobe,
   faCode,
   faCogs,
-  faExclamationTriangle
+  faExclamationTriangle,
+  faStar,
 } from '@fortawesome/free-solid-svg-icons';
 
 const articleType = (type) => {
@@ -20,11 +22,27 @@ const articleType = (type) => {
   return icon;
 };
 
-const TopicLink = ({data: { url, title, description, type} }) => {
+const TopicLink = ({data: { url, title, description, type, date } }) => {
+
+  let newHtml;
+  let isNew = false;
+
+  if(date) {
+    const postDate = moment(new Date(date));
+    const currentDate = moment(new Date());
+
+    const difference = currentDate.diff(postDate, 'days');
+
+    if(difference < 5) {
+      isNew = true;
+      newHtml = <div className="article-block__new"><FontAwesomeIcon icon={faStar} className="article-block__new-icon"/> New!</div>;
+    }
+  }
 
   return (
     <li className="col-12 col-md-6 col-md-4 mb-2">
       <a rel="noopener noreferrer" target="_blank" href={url} className="article-block">
+        { isNew && newHtml }
         <FontAwesomeIcon icon={articleType(type)} className={'article-block__icon'} />
         <div className="article-block__text">
           <p className="article-block__text-title">{title}</p>
